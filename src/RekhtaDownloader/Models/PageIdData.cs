f16e89f5eab1ab47426641d0509166aa525ad01d
Omar Faruk <omar.faruk@outlook.com>
2026-09-09 00:30:47 +0100

    Fixed downloaded pat 2

---
+++ src/RekhtaDownloader/Models/PageIdData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace RekhtaDownloader.Models
+{
+    public class PageIdData
+    {
+        public int From { get; set; }
+        public List<string> Ids { get; set; }
+    }
+}
